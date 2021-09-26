5743d992e86ae9a2eefce39118c629bee482e90e
DarciKim <91241383+DarciKim@users.noreply.github.com>
2021-09-26 20:41:59 +0900

    world 메소드 추가 (pull)

--- 01_hello_git/src/com/kh/git/HelloWorld.java
+++ 01_hello_git/src/com/kh/git/HelloWorld.java
@@ -22,9 +22,16 @@ public class HelloWorld {
 		new Cat().say();
 	}
 	/**
-	*원격저장소에서 수정 된 내용
+	*원격저장소에서 수정 된 내용 #1
 	*/
 	public void hello(){
 		System.out.println("저를 fetch 해 주세요.");
+	}
+	
+	/**
+	*원격저장소에서 수정 된 내용 #2
+	*/
+	public void world(){
+		System.out.println("저를 pull 해 주세요.");
 	}
 }
